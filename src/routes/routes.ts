baff4fb3ad66808ece8a959d59787160ad2d5738
kitan23 <ktran@hamilton.edu>
2023-04-27 12:05:02 -0400

    feat: get all upcoming events

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,11 @@
 import events from "./events";
 import eventSingle from "./eventSingle";
+import eventsUpcoming from "./eventsUpcoming";
 
 function routes(app: any) {
-    app.get("/events/:id", eventSingle);
-    app.get("/events", events);
+	app.get("/events/:id", eventSingle);
+	// app.get("/events/upcoming", eventsUpcoming);
+	app.get("/events", eventsUpcoming);
 }
 
 export default routes;
