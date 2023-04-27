8b1bae7f2ff012ef460a682d2cdc413381a0c446
Daren Hua <59425042+darenhua@users.noreply.github.com>
2023-04-27 01:42:21 -0400

    testing cicd

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,9 @@ import eventSingle from "./eventSingle";
 function routes(app: any) {
     app.get("/events/:id", eventSingle);
     app.get("/events", events);
+    app.get("/", (req: any, res: any) => {
+        res.json({ msg: "Ham Events App" });
+    });
 }
 
 export default routes;
