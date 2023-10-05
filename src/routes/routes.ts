1d6820205d9d00a5bde6c917230466a7a9febe75
Daren Hua <59425042+darenhua@users.noreply.github.com>
2023-10-05 11:41:44 -0400

    made update sync robust

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,11 @@
 import events from "./events";
 import eventSingle from "./eventSingle";
+import syncData from "../util/syncData";
 
 function routes(app: any) {
     app.get("/events/:id", eventSingle);
     app.get("/events", events);
+    app.get("/sync", syncData);
     app.get("/", (req: any, res: any) => {
         res.json({ msg: "Ham Events App" });
     });
