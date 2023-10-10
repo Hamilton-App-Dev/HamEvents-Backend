78899504a5d7d205076cc4f1247903f96a31b99e
Daren Hua <59425042+darenhua@users.noreply.github.com>
2023-10-10 01:11:15 -0400

    fix merge fail

--- src/25live/event.ts
+++ src/25live/event.ts
@@ -7,51 +7,51 @@ import getCurrentDate from "../util/time";
 import { DateDimensionField } from "aws-sdk/clients/quicksight";
 
 type IncompleteEvent = Omit<
-  Events,
-  "id" | "description" | "cover_img" | "cancelled"
+    Events,
+    "id" | "description" | "cover_img" | "cancelled"
 >;
 
 function normalize25Live(eventObj: any): IncompleteEvent {
-  let eventName: string = eventObj["event"]["event_name"];
+    let eventName: string = eventObj["event"]["event_name"];
 
-  let eventPostDate: Date = eventObj["post_event_dt"];
-  let eventLastModDate: Date = eventObj["last_mod_dt"];
-  let eventId: string = eventObj["reservation_id"];
-  let eventStart: Date = eventObj["event_start_dt"];
-  let eventEnd: Date = eventObj["event_end_dt"];
-  let organization: string =
-    eventObj["event"]["organization"]["organization_name"];
-  // event attendance is an optional field
-  let eventAttendance: number = Number(eventObj["expected_count"]) || 0;
-  let eventSpace: string =
-    eventObj["space_reservation"]["space"]["formal_name"];
-  let customAttribute: any = eventObj["event"]["custom_attribute"];
-  let eventFoodStatus: boolean = false;
-  for (let item of customAttribute) {
-    if (item["attribute_id"] == 100) {
-      if (item["attribute_value"] == "T") {
-        eventFoodStatus = true;
-      }
-      break;
+    let eventPostDate: Date = eventObj["post_event_dt"];
+    let eventLastModDate: Date = eventObj["last_mod_dt"];
+    let eventId: string = eventObj["reservation_id"];
+    let eventStart: Date = eventObj["event_start_dt"];
+    let eventEnd: Date = eventObj["event_end_dt"];
+    let organization: string =
+        eventObj["event"]["organization"]["organization_name"];
+    // event attendance is an optional field
+    let eventAttendance: number = Number(eventObj["expected_count"]) || 0;
+    let eventSpace: string =
+        eventObj["space_reservation"]["space"]["formal_name"];
+    let customAttribute: any = eventObj["event"]["custom_attribute"];
+    let eventFoodStatus: boolean = false;
+    for (let item of customAttribute) {
+        if (item["attribute_id"] == 100) {
+            if (item["attribute_value"] == "T") {
+                eventFoodStatus = true;
+            }
+            break;
+        }
     }
-  }
-  // console.log("ORGANIZATION: ", organization);
+    // console.log("ORGANIZATION: ", organization);
 
-  let newEvent: IncompleteEvent = {
-    ref_id: eventId,
-    name: eventName,
-    organization: organization,
-    event_time_start: eventStart,
-    event_time_end: eventEnd,
-    event_post_date: eventPostDate,
-    event_last_modified: eventLastModDate,
-    estimated_attendance: eventAttendance,
-    location: eventSpace,
-    food: eventFoodStatus,
-    modified: false,
-  };
+    let newEvent: IncompleteEvent = {
+        ref_id: eventId,
+        name: eventName,
+        organization: organization,
+        event_time_start: eventStart,
+        event_time_end: eventEnd,
+        event_post_date: eventPostDate,
+        event_last_modified: eventLastModDate,
+        estimated_attendance: eventAttendance,
+        location: eventSpace,
+        food: eventFoodStatus,
+        modified: false,
+    };
 
-  return newEvent;
+    return newEvent;
 }
 
 const removeDuplicates = (eventsList: IncompleteEvent[]) => {
@@ -78,33 +78,35 @@ const removeDuplicates = (eventsList: IncompleteEvent[]) => {
 };
 
 const getRelevantEvents = async (apiToken: string) => {
-  let currentDate: string = getCurrentDate();
-  let server: string = `https://apim.workato.com/hamiltonbi/25live-v1/student-events-for-app-api?start_dt=${currentDate}&end_dt=20240501&modified_since=20220101T00:00:00`;
-  let eventsList: IncompleteEvent[] = [];
+    let currentDate: string = getCurrentDate();
+    let server: string = `https://apim.workato.com/hamiltonbi/25live-v1/student-events-for-app-api?start_dt=${currentDate}&end_dt=20240501&modified_since=20220101T00:00:00`;
+    let eventsList: IncompleteEvent[] = [];
 
-  try {
-    const res = await fetch(server, {
-      method: "GET",
-      headers: {
-        "API-TOKEN": apiToken,
-      },
-    });
+    try {
+        const res = await fetch(server, {
+            method: "GET",
+            headers: {
+                "API-TOKEN": apiToken,
+            },
+        });
 
-    const json = await res.json();
-    // console.log(json);
-    let reservationList = json["reservations"]["reservation"];
-    for (let eventObj of reservationList) {
-      const newEvent = normalize25Live(eventObj);
-      eventsList.push(newEvent);
-      for (let item of eventObj["event"]["custom_attribute"]) {
-        // 49 is id for invite only
-        if (item["attribute_id"] == 49) {
-          if (item["attribute_value"] == "F") {
+        const json = await res.json();
+        // console.log(json);
+        let reservationList = json["reservations"]["reservation"];
+        for (let eventObj of reservationList) {
             const newEvent = normalize25Live(eventObj);
             eventsList.push(newEvent);
-            // console.log(eventObj["event"]["custom_attribute"]);
-          }
-          break;
+            for (let item of eventObj["event"]["custom_attribute"]) {
+                // 49 is id for invite only
+                if (item["attribute_id"] == 49) {
+                    if (item["attribute_value"] == "F") {
+                        const newEvent = normalize25Live(eventObj);
+                        eventsList.push(newEvent);
+                        // console.log(eventObj["event"]["custom_attribute"]);
+                    }
+                    break;
+                }
+            }
         }
         eventsList = removeDuplicates(eventsList);
         return eventsList;
@@ -112,7 +114,6 @@ const getRelevantEvents = async (apiToken: string) => {
         console.log(error);
         return eventsList;
     }
-
 };
 
 export { getRelevantEvents, IncompleteEvent };
